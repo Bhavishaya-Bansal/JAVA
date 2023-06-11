32243d378e124f4e8d67e2bee2661e6fee8f1454
Your Name <you@example.com>
2023-06-11 14:24:18 +0530

    new

--- practise.java
+++ practise.java
@@ -3,17 +3,18 @@ import java.util.*;
 class practise{
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
+        int count= 0;
+    
         int n= sc.nextInt();
-        int f= 0;
-        int s= 1;
-        int count= 2;
-        while(count<= n){
-            int temp =s;
-            s = s+f;
-            f= temp;
-            count++;
+        while(n>0){
+            int rem= n% 10;
+            if(rem == 7){
+                count++;
+            }
+            n= n/10;
         }
-        System.out.println(s);
+
+        System.out.println(count);
 
     }
 }
