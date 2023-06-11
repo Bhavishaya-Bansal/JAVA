515d7004d25f3f4b42198b2288f5b6cb442430a1
Your Name <you@example.com>
2023-06-11 15:23:29 +0530

    11 june

--- practise.java
+++ practise.java
@@ -3,18 +3,54 @@ import java.util.*;
 class practise{
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
-        int count= 0;
-    
-        int n= sc.nextInt();
-        while(n>0){
-            int rem= n% 10;
-            if(rem == 7){
-                count++;
+        int ans= 0;
+
+        
+        while(true){
+            
+            System.out.print("enter the operator: ");
+            char op= sc.next().trim().charAt(0);
+
+            if(op== '-' || op=='+' || op== '*' || op== '/' || op == '%'){
+                System.out.println("Enter the 2 numbers: ");
+                int num1 = sc.nextInt();
+                int num2 = sc.nextInt();
+                if(op== '+'){
+                    ans = num1+ num2;            
+                }
+                if(op== '-'){
+                    ans = num1- num2;            
+                }
+                if(op== '*'){
+                    ans = num1* num2;            
+                }
+                if(op== '/'){
+                    if(num2!=0){
+                        ans= num1 / num2;
+                    }
+                    else{
+                        break;
+                    }
+                }
+                if(op== '%'){
+                    if(num2!=0){
+                        ans= num1 % num2;
+                    }
+                    else{
+                        break;
+                    }
+                }
             }
-            n= n/10;
+            else if(op == 'x'|| op == 'X'){
+                break;
+            }
+            else{
+                System.out.println("Invalid operator");
+                break;
+            }
+            System.out.println(ans);
         }
 
-        System.out.println(count);
 
     }
 }
