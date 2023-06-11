0ecc6f7b7b22d4e37bcf7fe959e7d35372f4248a
Your Name <you@example.com>
2023-06-11 20:06:57 +0530

    11 june

--- SwitchCases.java
+++ SwitchCases.java
@@ -19,7 +19,6 @@ public class SwitchCases {
                 break;
             
             case "Grapes":
-            // this runs gor grapes 
                 System.out.println("Small fruits");
                 break;
 
@@ -27,5 +26,14 @@ public class SwitchCases {
                 System.out.println("Please choose another fruit.");
 
         }
+
+        
+        switch(fruit){ // Enhanced switch function
+            case "Mango" -> System.out.println("The king of fruits");
+            case "Apple" -> System.out.println("Juicy red fruit");
+            case "Orange" -> System.out.println("Round fruit");
+            case "Grapes" -> System.out.println("Small fruits");
+        } 
+        // this is the code for enhanced switch cases
     }
 }
