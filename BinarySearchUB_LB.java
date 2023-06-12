f4e62e8708c76e2ff32dbb7df9e60fd0709240d3
bansalbhavishaya <bhavishayabansal31@gmail.com>
2023-06-12 11:45:01 +0530

    12 june

--- BinarySearchUB_LB.java
+++ BinarySearchUB_LB.java
@@ -1,9 +1,35 @@
 import java.util.*;
 
-import javax.swing.text.html.HTMLDocument.RunElement;
-
 public class BinarySearchUB_LB {
-    
+    // This code is for Binary search
+    // Lower Bound: This means that the function returns the index of the next smallest number just greater than or equal to that number. If there are multiple values that are equal to the number, lower_bound() returns the index of the first such value.
+
+    /*  Example: 
+    Input  : 4 6 10 12 18 18 20 20 30 45
+    Output : lower_bound for element 18 at index 4
+
+    Input  : 4 6 10 12 16 20 28
+    Output : lower_bound for element 18 at index 5
+
+    Input  : 24 26 40 56
+    Output : lower_bound for element 18 at index 0
+
+    Input  : 4 6 10 12 16 17
+    Output : lower_bound for element 18 at index 6               */
+
+
+    // Upper Bound: It returns the index of the first element in the array which has a value greater than key or last if no such element is found. If the element is bigger than the last element of the array then it will return zero OR upper bound does not exist
+
+    /*  Example:
+    Input  : 10 20 30 30 40 50
+    Output : upper_bound for element 30 is 40 at index 4
+
+    Input  : 10 20 30 40 50
+    Output : upper_bound for element 45 is 50 at index 4
+
+    Input  : 10 20 30 40 50
+    Output : upper_bound for element 60 does not exists           */
+
 
     public static int lowerBound(int arr[], int target){
         int low= 0;
