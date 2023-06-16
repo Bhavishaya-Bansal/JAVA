74ac388c1d91d3d2e122fdb32b829adbbd7b1e9c
bansalbhavishaya <bhavishayabansal31@gmail.com>
2023-06-16 10:20:32 +0530

    16 june

--- BinarySearchUB_LB.java
+++ BinarySearchUB_LB.java
@@ -64,10 +64,64 @@ public class BinarySearchUB_LB {
                 high = mid-1;
             }
         }
-        return low; // because it will be the just bigger element which we will need as ans
+        return arr[low]; // because it will be the just bigger element which we will need as ans
+    }
+
+
+    // code to find ceiling of a number.. kunal kushwaha style
+    public static int ceilingOfNumber(int arr[], int target) {
+        int low= 0;
+        int high = arr.length-1;
+
+        if(target > arr[high]){
+                return -1;
+        }
+
+        while(low<= high){
+            int mid= low+ (high- low)/2;
+            if(arr[mid]== target){
+                return mid;
+            }
+            else if(arr[mid]< target){
+                low= mid+ 1;
+    
+            }
+            else if(arr[mid]> target){
+                high = mid-1;
+            }
+        }
+        return arr[low];
+    }
+
+    public static int floorOfNumber(int arr[], int target){
+        int low =0; 
+        int high= arr.length- 1;
+        
+        if(target< arr[low]){
+            return -1;
+        }
+
+        while(low<= high){
+            int mid = low+ (high- low)/2;
+
+            if(arr[mid]== target){
+                return mid;
+            }
+
+            else if(arr[mid]< target){
+                low = mid+ 1;
+            }
+            else{
+                high = mid- 1;
+            }
+        }
+        return arr[high]; 
     }
 
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        int[] arr = {1, 2, 8, 10, 10, 12, 19};
+        int target= 11;
+        System.out.println("ans "+ floorOfNumber(arr, target));
     }
 }
--- practise.java
+++ practise.java
@@ -3,32 +3,28 @@ import java.util.*;
 class practise{
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
-        int arr[][]= {
-            {23, 4, 1, 54},
-            {65, 34, 0},
-            {6543, 5, 99},
-            {23, 65}
-        };
-        int target = 100;
-        int ans[] = searchInMatrix(arr, target);
-        System.out.println(Arrays.toString(ans));
+        int arr[]= {23, 4, 1, 54}; 
+        System.out.println(findNumbers(arr));
     }
 
-    static int[] searchInMatrix(int arr[][], int target){
-        if(arr.length==0){
-            return new int[]{0,0};
+    static int findNumbers(int[] nums) {
+        int counter=0;
+        for(int i=0; i<nums.length; i++){
+            if(isEven(nums[i]));
+            counter++;
         }
+        return counter;
+    }
 
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[0].length; j++){
-                if(arr[i][j] == target){
-                    return new int[]{i, j};
-                }
-                else{
-                    return new int[]{-1,-1};
-                }
-            }
+    static boolean isEven(int n){
+        int count =0;
+        while(n> 0){
+            count++;
+            n= n/10;
         }
-        return new int[]{-1,-1};
+        if(count %2 == 0){
+            return true;
+        }
+        return false;
     }
 }
