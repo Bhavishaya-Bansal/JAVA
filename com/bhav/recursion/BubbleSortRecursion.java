678db19c6c4e13debfc35dc6bcecc252697761b6
bansalbhavishaya <bhavishayabansal31@gmail.com>
2023-06-29 23:28:34 +0530

    update

--- com/bhav/recursion/BubbleSortRecursion.java
+++ com/bhav/recursion/BubbleSortRecursion.java
@@ -1,6 +1,32 @@
 package com.bhav.recursion;
 
+import java.util.Arrays;
+
 // Here we will be doing BUBBLE SORT USING RECURSION..
 public class BubbleSortRecursion {
-    
+    public static void main(String[] args) {
+        int arr[]= {5, 23, 6, 1, 7, 3, 9};
+        sort(arr, arr.length-1, 0);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+
+    // doing it the same way the pattern 1 was printed.. just adding the swap thing in between..
+    public static void sort(int arr[], int row, int colm){
+        if(row== 0){
+            return ;
+        }
+        if(colm< row){
+            if(arr[colm]> arr[colm+1]){
+                int temp= arr[colm];
+                arr[colm]= arr[colm+1];
+                arr[colm+1]= temp;
+            }
+            sort(arr, row, colm+1);
+        }
+
+        else{
+            sort(arr, row-1, 0);
+        }
+    }
 }
