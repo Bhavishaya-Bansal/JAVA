ad509263e77f5f8307caa6ed922dd57b2878dac9
bansalbhavishaya <bhavishayabansal31@gmail.com>
2023-07-26 22:37:32 +0530

    26 july

--- com/bhav/Hash/ItenaryPrblm.java
+++ com/bhav/Hash/ItenaryPrblm.java
@@ -2,7 +2,7 @@ package com.bhav.Hash;
 
 import java.util.*;
 
-public class HashProblems {
+public class ItenaryPrblm{
 public static void main(String[] args) {
         HashMap<String, String> tickets= new HashMap<>();
         tickets.put("Chennai", "Bengaluru");
