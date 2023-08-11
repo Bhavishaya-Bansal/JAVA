675af14ad748fc501e3971e93a08fdd2173bcf40
bansalbhavishaya <bhavishayabansal31@gmail.com>
2023-08-12 01:35:40 +0530

    12 august

--- com/bhav/TreesKK/BinarySearchTree.java
+++ com/bhav/TreesKK/BinarySearchTree.java
@@ -47,6 +47,37 @@ public class BinarySearchTree {
         return false;
     }
 
+    // Inserting in our Binary Search Tree..
+    public void insert(int value){
+        // while coming out of recursion calls in the end root will be returned so we will assign all the changes to the root node only..
+        root= insertIn(value, root);
+
+    }
+
+    private Node insertIn(int value, Node node){
+        // if our node becomes null(means we are inserting a node below of our leaf node).. then we have to create a new node there and return it..
+        if(node== null){
+            node= new Node(value);
+            return node;
+        }
+
+        // if our 'value' of the new node that we are inserting is less than than the current node that we are at.. then insert it to it's left.. 
+        if(value< node.value){
+            node.left= insertIn(value, node.left);
+        }
+
+        // else.. if the value is greater we will add it to the right.. 
+        if(value> node.value){
+            node.right= insertIn(value, node.right);
+        }
+
+        // now while we are getting out of recursion call, as we are adding a new node the height of the node will be changed so.. we have to change the height of each node to be.. max between the left height and the right height of that node and that would be increased by one(as the max of left and right side would give us the height of the child node of the current node at which we are at)..
+        node.height= Math.max(height(node.left), height(node.right))+ 1;
+
+        // whenever we are getting out of recursion call we will return the nodes that were at the posn earlier and were called in the recursion stack as it is.. we will do so because if we will not do so our tree will change
+        return node; 
+    }
+
     // To display our tree..
     public void display(){
         // calling the recursive function to display our tree
