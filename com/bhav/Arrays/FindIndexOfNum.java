ee94d3b5931bb909083f22d35e6143537c0227cf
bansalbhavishaya <bhavishayabansal31@gmail.com>
2023-09-14 00:21:10 +0530

    14 sep

--- com/bhav/Arrays/FindIndexOfNum.java
+++ com/bhav/Arrays/FindIndexOfNum.java
@@ -2,25 +2,32 @@ package com.bhav.Arrays;
 
 import java.util.*;
 
-//Finding the index of the number user is entering
+//Finding the index of the number user is entering.. Using Linear Search..
 public class FindIndexOfNum{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int arr[]= new int[size];
+        boolean flag= false;
 
         for(int i=0; i<size; i++){
             arr[i]=sc.nextInt();
         }
         
-        System.out.println("enter the number you want to search ");
+        System.out.println("Enter the number you want to search: ");
         int num = sc.nextInt();
 
-        for(int i =0;i<size;i++){
+        // Printing the first index where the target appears ..
+        for(int i= 0; i< size; i++){
             if(arr[i]==num){
                 System.out.println(i);
+                flag= true;
                 break;
             }
+        }
+
+        if(flag== false){
+            System.out.println("Not present in the array");
         }
         sc.close();
     }
