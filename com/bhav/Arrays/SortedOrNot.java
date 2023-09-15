532a5461003d2ab67403179de566f03eeaaf02bc
bansalbhavishaya <bhavishayabansal31@gmail.com>
2023-09-15 18:47:14 +0530

    15 sep

--- com/bhav/Arrays/SortedOrNot.java
+++ com/bhav/Arrays/SortedOrNot.java
@@ -17,7 +17,8 @@ public class SortedOrNot{
         boolean isAscending = true;
         for(int i = 0; i<size-1; i++){
             if(arr[i]>arr[i+1]){
-                isAscending = false;    
+                isAscending = false;
+                break;
             }
         }
 
