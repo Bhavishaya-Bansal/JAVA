81e59bfcf92c4fd0133cd19486bb6b38c51c7377
bansalbhavishaya <bhavishayabansal31@gmail.com>
2025-08-13 21:33:02 +0530

    Practise

--- practise.java
+++ practise.java
@@ -3,11 +3,19 @@ import java.util.*;
 public class Practise{
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
-        // int n= sc.nextInt();
-        // String name= sc.nextLine();
-        float marks= sc.nextFloat();
-        int num= (int)(56.54);
-        System.out.println(marks);
+        char ch= sc.next().trim().charAt(0);
+        System.out.println(ch);
+        // int a= (int)ch;
 
+        if(ch >= 'a' && ch<= 'z' ){
+            System.out.println("Lower");
+        }
+        else if(ch >= 'A' && ch<= 'Z'){
+            System.out.println("Upper");
+        }
+        else{
+            System.out.println("Not a char");
+        }
+        // System.out.println(a);
     }
 }
